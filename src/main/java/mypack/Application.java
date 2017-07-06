68e61cddae5336a4bda9d1fd63d11898fb43d64d
pranavkhurana <pranavkhurana96@gmail.com>
2017-07-06 22:12:01 +0530

    final changes for Q6 branch

--- src/main/java/mypack/Application.java
+++ src/main/java/mypack/Application.java
@@ -11,22 +11,22 @@ public class Application {
         //Solution 6
         System.out.println("Solution 6");
         Restaurant restaurant1=context.getBean("restaurant1",Restaurant.class);
-        // Restaurant restaurant2=context.getBean("restaurant2",Restaurant.class);
+        Restaurant restaurant2=context.getBean("restaurant2",Restaurant.class);
         Restaurant restaurant3=context.getBean("restaurant3",Restaurant.class);
 
         restaurant1.getHotDrink().prepareHotDrink();
-        //restaurant2.getHotDrink().prepareHotDrink();
+        restaurant2.getHotDrink().prepareHotDrink();
         restaurant3.getHotDrink().prepareHotDrink();
 
         //Solution 7
         System.out.println("\nSolution 7");
         Restaurant teaRestaurant=context.getBean("teaRestaurant",Restaurant.class);
 
-        //hot drind of teaRestaurant changed
+        //hot drink of teaRestaurant changed
         teaRestaurant.setHotDrink(new ExpressTea());
         teaRestaurant.getHotDrink().prepareHotDrink();
 
-        //another instance of teaRestaurant created using getBean
+        //another instance of teaRestaurant created using getBean and its hotDrink is checked
         Restaurant teaRestaurant1=context.getBean("teaRestaurant",Restaurant.class);
         teaRestaurant1.getHotDrink().prepareHotDrink();
 
